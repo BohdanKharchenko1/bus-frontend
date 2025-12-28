9d19cf2acf0bc86a4defb5cd19fffcec83f86e66
Bohdan <84513013+naughtyCURSED@users.noreply.github.com>
2025-12-28 16:03:05 +0100

    vercel fix

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,39 +1,41 @@
 import Navbar from '../components/partials/Navbar.tsx';
 import PromoSection from '../components/partials/PromoSection.tsx';
 import Footer from '../components/partials/Footer.tsx';
-import Header from "../components/partials/Header.tsx";
-import About from "../components/partials/About.tsx";
-import Reviews from "../components/partials/Reviews.tsx";
-import Services from "../components/partials/Services.tsx";
+import Header from '../components/partials/Header.tsx';
+import About from '../components/partials/About.tsx';
+import Reviews from '../components/partials/Reviews.tsx';
+import Services from '../components/partials/Services.tsx';
 import Step1 from '../components/booking/Step1.tsx';
+import { useBookingStore } from '@/stores/bookingStore.ts';
 
 export default function MainPage() {
-    return (
-        <>
-            <Navbar />
+  const nextStep = useBookingStore((s) => s.nextStep);
+  return (
+    <>
+      <Navbar />
 
-            {/* HEADER */}
-            <div className="relative">
-                <Header />
+      {/* HEADER */}
+      <div className="relative">
+        <Header />
 
-                <div
-                    className="
+        <div
+          className="
             relative z-30
             -mt-24 sm:-mt-32 md:-mt-40 lg:-mt-48
           "
-                >
-                    <Step1 />
-                </div>
-            </div>
+        >
+          <Step1 onNext={nextStep} />
+        </div>
+      </div>
 
-            <div className="relative z-10 -mt-12 sm:-mt-16 md:-mt-20 lg:-mt-24">
-                <Services />
-            </div>
+      <div className="relative z-10 -mt-12 sm:-mt-16 md:-mt-20 lg:-mt-24">
+        <Services />
+      </div>
 
-            <About />
-            <Reviews />
-            <PromoSection />
-            <Footer />
-        </>
-    );
+      <About />
+      <Reviews />
+      <PromoSection />
+      <Footer />
+    </>
+  );
 }
